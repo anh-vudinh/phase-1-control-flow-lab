1992ce40ed00055cb18d30c7ff0722bd49705eb3
Anh-vu <anhuvdinh@gmail.com>
2021-08-09 16:41:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-function scuberGreetingForFeet(){
+let message;
+let distance;
+let city;
+let tip;
+
+scuberGreetingForFeet();
+ternaryCheckCity();
+switchOnCharmFromTip();
+
+function scuberGreetingForFeet(distance, message){
   // Write your code here!
+
+  if(distance <= 400){
+    message = "This one is on me!";
+    return message;
+  } else if (distance > 2000 && distance <= 2500){
+    message = 'I will gladly take your thirty bucks.';
+    return message;
+  } else if (distance > 2500){
+    message = `No can do.`;
+    return message;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city, message){
   // Write your code here!
+  if(city == "NYC"){
+    message = "Ok, sounds good."
+    return message;
+  } else{
+    message = "No go."
+    return message;
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip, message){
   // Write your code here!
+    switch(tip) {
+      case "generous":
+        message = "Thank you so much.";
+        return message;
+        break;
+      case "not as generous":
+        message = "Thank you.";
+        return message;
+        break;
+      default:
+        message = "Bye.";
+        return message;
+  }
 }
